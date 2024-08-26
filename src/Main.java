162950623589388a639af302b5458314885e6bf7
foagura <foagura_0416@yahoo.co.jp>
2024-08-26 21:13:27 +0900

    配列について

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+/*　javaの配列についての学習　*/
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        int[] scores;
+        scores = new int[3];
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        scores[0] = 1;
+        scores[1] = 5;
+        scores[2] = 10;
+
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+
+        /*　配列の複数の記述方法　*/
+        int[] scores2 = new int[3];
+
+        var scores3 = new int[3];
+
+        int[] scores4 = {1, 2, 3};
     }
 }
